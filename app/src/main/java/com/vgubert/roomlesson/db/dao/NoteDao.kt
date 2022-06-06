791acdd413866ec0d0a6cbf0448fe58ae8f0a126
Vyacheslav <v.guber2000@gmail.com>
2022-06-06 23:04:47 +0300

    Fill in the Dao, Repository and Database

--- app/src/main/java/com/vgubert/roomlesson/db/dao/NoteDao.kt
+++ app/src/main/java/com/vgubert/roomlesson/db/dao/NoteDao.kt
@@ -1,4 +1,18 @@
 package com.vgubert.roomlesson.db.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.vgubert.roomlesson.model.NoteModel
+
+@Dao
 interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(noteModel: NoteModel)
+
+    @Delete
+    suspend fun delete(noteModel: NoteModel)
+
+    @Query("SELECT * FROM note_table")
+    fun getAllNotes(): LiveData<List<NoteModel>>
 }
--- app/src/main/java/com/vgubert/roomlesson/db/repository/NoteRepository.kt
+++ app/src/main/java/com/vgubert/roomlesson/db/repository/NoteRepository.kt
@@ -1,4 +1,10 @@
 package com.vgubert.roomlesson.db.repository
 
+import androidx.lifecycle.LiveData
+import com.vgubert.roomlesson.model.NoteModel
+
 interface NoteRepository {
+    val allNotes: LiveData<List<NoteModel>>
+    suspend fun insertNote(noteModel: NoteModel, onSuccess:() -> Unit)
+    suspend fun deleteNote(noteModel: NoteModel, onSuccess:() -> Unit)
 }
